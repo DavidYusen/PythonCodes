064ba1f505ca8f9d8a2c56e80f441c62b8a18e27
FreshMeat <yusende@hotmail.com>
2019-09-08 13:26:57 +0800

    remove useless code

--- CheckAvaliableDomain/run.py
+++ CheckAvaliableDomain/run.py
@@ -3,6 +3,7 @@
 import os
 
 
+# 从文件中读取每行的字符串作为域名
 def generate_list():
     urllist = []
     f1 = open('words.txt', 'r')
@@ -10,26 +11,35 @@ def generate_list():
         urllist.append(x.replace("\n", ""))
     return urllist
 
-def generate_list2():
-    wlist='abcdefghijklmnopqrst'
-    urlist=[m + n + o for m in wlist for n in wlist for o in wlist]
-    return urlist
 
-
+# 生成任意三个字母组合的域名
 def generate_list1():
-    urlist = ['abc','bcd','adfed','aaaaaa99d9998']
+    wlist = 'abcdefghijklmnopqrst'
+    urlist = [m + n + o for m in wlist for n in wlist for o in wlist]
     return urlist
 
 
-urllist = generate_list()
-f = open("result.txt", 'w')
+# 任两个字符串的组合作为域名
+def generate_list2():
+    urllist = []
+    f1 = open('chnlist.txt', 'r')
+    for x in f1.readlines():
+        urllist.append(x.replace("\n", ""))
+    resultlist = [m + n for m in urllist for n in urllist]
+    return resultlist
 
-for urlitem in urllist:
-    cmd = "nslookup www.%s.com" % urlitem
-    with os.popen(cmd, "r") as p:
-        result = p.read()
 
-    if urlitem not in result:
-        f.write(urlitem)
-        f.write('\n')
-f.close()
+if __name__ == '__main__':
+    urllist = generate_list()
+    f = open("result.txt", 'w')
+
+    for urlitem in urllist:
+        cmd = "nslookup www.%s.com" % urlitem
+        with os.popen(cmd, "r") as p:
+            result = p.read()
+
+        if urlitem not in result:
+            f.write(urlitem)
+            f.write('\n')
+
+    f.close()
